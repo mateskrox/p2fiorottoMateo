ec7dabb31042bd71c116c18e5b741dbd8cb75fb0
Mateo Fiorotto <112798055+mateskrox@users.noreply.github.com>
2023-04-13 21:29:41 -0300

    Add files via upload

---
+++ TP1/JS/funciones.js
@@ -0,0 +1,7 @@
+alert("Hola Mundo");
+console.log(23+11);
+console.log("Bienvenidos a la consola");
+function MostrarValor() {
+    const aux= document.getElementById("valor");
+    console.log(aux.value);
+}
